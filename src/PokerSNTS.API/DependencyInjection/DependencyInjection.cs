23a04d7187fb533ec0df882a0c23342aa48de5f8
willian Hilton <willian.hilton@hotmail.com>
2021-03-27 19:06:45 -0300

    feat: Resolvendo Injeção de dependencia na API

---
+++ src/PokerSNTS.API/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PokerSNTS.Domain.Interfaces.Repositories;
+using PokerSNTS.Domain.Interfaces.Services;
+using PokerSNTS.Domain.Interfaces.UnitOfWork;
+using PokerSNTS.Domain.Notifications;
+using PokerSNTS.Domain.Services;
+using PokerSNTS.Infra.Data.Contexts;
+using PokerSNTS.Infra.Data.Repositories;
+using PokerSNTS.Infra.Data.UnitOfWork;
+
+namespace PokerSNTS.API
+{
+    public static class DependencyInjection
+    {
+        public static void ResolveDependencyInjection(this IServiceCollection services)
+        {
+            //Notification
+            services.AddScoped<IDomainNotificationHandler, DomainNotificationHandler>();
+
+            //Data
+            services.AddScoped<PokerContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IPlayerRepository, PlayerRepository>();
+            services.AddScoped<IPlayerRoundRepository, PlayerRoundRepository>();
+            services.AddScoped<IRankingRepository, RankingRepository>();
+            services.AddScoped<IRankingPunctuationRepository, RankingPunctuationRepository>();
+            services.AddScoped<IRegulationRepository, RegulationRepository>();
+            services.AddScoped<IRoundRepository, RoundRepository>();
+
+            //Service
+            services.AddScoped<IPlayerService, PlayerService>();
+            services.AddScoped<IPlayerRoundService, PlayerRoundService>();
+            services.AddScoped<IRankingService, RankingService>();
+            services.AddScoped<IRankingPunctuationService, RankingPunctuationService>();
+            services.AddScoped<IRegulationService, RegulationService>();
+            services.AddScoped<IRoundService, RoundService>();
+        }
+    }
+}
--- src/PokerSNTS.API/Startup.cs
+++ src/PokerSNTS.API/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using PokerSNTS.Infra.Data.Contexts;
 
 namespace PokerSNTS.API
 {
@@ -23,18 +18,20 @@ namespace PokerSNTS.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.ResolveDependencyInjection();
+
+            services.AddDbContext<PokerContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("PokerSNTS")));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PokerSNTS.API", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
